d005b0717765cabfff66a81e174043a55a4f77fc
Mulay <Mulay@DESKTOP-1DQT6OU.home>
2021-02-24 13:09:34 +0100

    Encontrar numeros repetidos en una lista.

--- src/algoritmoflyod/AlgoritmoFlyod.java
+++ src/algoritmoflyod/AlgoritmoFlyod.java
@@ -5,17 +5,47 @@
  */
 package algoritmoflyod;
 
+
 /**
  *
  * @author Mulay
  */
 public class AlgoritmoFlyod {
 
+        /**
+     * Encuentra el número qu está duplicado en una lista de ints
+     * usando el algoritmo de Floyd
+     * @param listaNumeros
+     * @return 
+     */
+    // [1, 3, 2, 4, 4]
+    public int findDuplicate(int [] listaNumeros){
+        int tortuga = listaNumeros[0];
+        int liebre = listaNumeros[0];
+        boolean iguales = false;
+        while (!iguales){
+            tortuga = listaNumeros[tortuga];
+            liebre = listaNumeros[listaNumeros[liebre]];
+            if (tortuga == liebre){
+                iguales = true;
+            }
+        }
+        int aux1 = listaNumeros[0];
+        int aux2 = tortuga;
+        while (aux1 != aux2){
+            aux1 = listaNumeros[aux1];
+            aux2 = listaNumeros[aux2];
+        }
+        return aux1;
+    }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        AlgoritmoFlyod ejercicio = new AlgoritmoFlyod();
+        System.out.println(ejercicio.findDuplicate(new int[] {3,2,3,4,1}));
+        System.out.println(ejercicio.findDuplicate(new int[] {4,2,3,4,1}));
+        System.out.println(ejercicio.findDuplicate(new int[] {1,1,3,4,1}));
     }
     
 }
